8d2f8ae9a75b768421630644339c15dbe815e501
Dave <dthompson@journaltech.com>
2025-12-05 09:39:40 -0800

    Initial commit: GIA Studio with TipTap editor

--- src/components/Editor/TextEditor.tsx
+++ src/components/Editor/TextEditor.tsx
@@ -41,6 +41,7 @@ export function TextEditor() {
                 editor.commands.setContent(newHtml);
             }
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [editor, page?.pageNumber]);
 
     if (!editor || !page) return null;
